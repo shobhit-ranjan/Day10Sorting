293f1b623c3338a841ba49f8316b8c31e3cfb5a5
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-07 08:00:57 +0530

    Added

--- ThreeInputFrUser.sh
+++ ThreeInputFrUser.sh
@@ -18,3 +18,17 @@ echo "Result is First exp is" $d
 echo "Result of second exp is" $e
 echo "Result of Third exp is" $f
 echo "Result of fouth exp is" $g
+
+
+declare -A dicti
+
+dicti[exp1]=$d
+
+dicti[exp2]=$e
+
+dicti[exp3]=$f
+
+dicti[exp4]=$g
+
+
+echo ${dicti[@]}
