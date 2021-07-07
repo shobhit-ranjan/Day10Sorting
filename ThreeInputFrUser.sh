670ede7d683f25feda6c76ca1082f64babda0991
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-07 07:48:24 +0530

    Added

--- ThreeInputFrUser.sh
+++ ThreeInputFrUser.sh
@@ -13,6 +13,8 @@ echo " input are "$a $b $c
 d=$(($a+$b*$c))
 e=$(($a$b+$c))
 f=$(($c+$a/$b))
+g=$(($a%$b+$c))
 echo "Result is First exp is" $d
 echo "Result of second exp is" $e
 echo "Result of Third exp is" $f
+echo "Result of fouth exp is" $g
